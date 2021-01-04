4095f94ff2b73f230e70574a0615b81e2c78a85d
Anepsios <Anepsios@users.noreply.github.com>
2021-01-04 22:21:16 +0200

    -Added product model

--- Startup.cs
+++ Startup.cs
@@ -42,10 +42,10 @@ namespace MvcUserAndRoles
                 }
             }
 
-            if (!roleManager.RoleExists("Manager"))
+            if (!roleManager.RoleExists("Customer"))
             {
                 var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
-                role.Name = "Manager";
+                role.Name = "Customer";
                 roleManager.Create(role);
             }
 
